1b82e1e33c64249746d35bcb01fa03fcdcf4e96f
Carly Seitz <carlyseitz817@gmail.com>
2019-05-23 15:40:40 -0500

    copy of latest push to master aka save-to-database

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "04defd9769a033ddb8b5";
+/******/ 	var hotCurrentHash = "f2f117bd31e2191d2df4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- server/services/auth.js
+++ server/services/auth.js
@@ -10,13 +10,12 @@ exports.checkJWT = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 50,
-    jwksUri: 'https://eincode.eu.auth0.com/.well-known/jwks.json'
+    jwksUri: 'https://dev-ofwx6q85.auth0.com/.well-known/jwks.json'
   }),
-  audience: 'NfvS9nw81ItncHJKPHCaAvwD9ChNWYn3',
-  issuer: 'https://eincode.eu.auth0.com/',
+  audience: '7ZE6aNVCenqc2Ghy21fA7VcWbzcgPEWz',
+  issuer: 'https://dev-ofwx6q85.auth0.com/',
   algorithms: ['RS256']
 })
-
 
 exports.checkRole = role => (req, res, next) => {
   const user = req.user;
@@ -24,7 +23,7 @@ exports.checkRole = role => (req, res, next) => {
   if (user && user[NAMESPACE + '/role'] && (user[NAMESPACE + '/role'] === role)) {
     next();
   } else {
-    return res.status(401).send({title: 'Not Authorized', detail: 'You are not authorized to access this data'})
+    return res.status(401).send({ title: 'Not Authorized', detail: 'You are not authorized to access this data' })
   }
 }
 
