106a4377687777f1e0c78bb88785568e886422e3
Carly Seitz <carlyseitz817@gmail.com>
2019-07-17 14:20:30 -0500

    make sure cookie code in _app.js stays

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import cookies from 'next-cookies';
+import nookies from 'nookies';
+import last from '../services/lastPage';
 import { ToastContainer } from 'react-toastify';
+import Cookies from 'js-cookie';
+import Router, { withRouter } from 'next/router';
 
 import auth0 from '../services/auth0';
 
@@ -10,10 +15,28 @@ import '../styles/main.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
 class MyApp extends App {
+  // constructor(props) {
+  //   super(props);
+
+  constructor({ router }) {
+    super()
+
+    this.state = {
+      activeURL: router.asPath,
+      activeURLTopLevelRouterDeprecatedBehavior: router.asPath,
+      activeURLTopLevelRouter: router.asPath
+    }
+
+    Router.events.on('routeChangeStart', (url) => {
+      console.log("routeChangeStart executed")
+
+      console.log(url)
+    })
+  }
+
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {}
 
-    // const isAuthenticated = process.browser ? auth0.clientAuth() : auth0.serverAuth(ctx.req);
     const user = process.browser ? await auth0.clientAuth() : await auth0.serverAuth(ctx.req);
 
     if (Component.getInitialProps) {
@@ -22,17 +45,72 @@ class MyApp extends App {
 
     const auth = { user, isAuthenticated: !!user };
 
-    return { pageProps, auth };
+    // const lastPage = process.browser ? await last.clientLastPage() : await last.serverLastPage(ctx.req)
 
+    // return { pageProps, auth, lastPage };
+
+    return { pageProps, auth };
+  }
+
+  // handleRouteChange(url) {
+  //   Cookies.set("AUTH0_STATE", JSON.stringify(window.location.href));
+
+  //   console.log('App is changing to: ', url)
+  // }
+
+  componentDidMount() {
+    Router.onRouteChangeComplete = this.handleRouteChangeTopLevelRouterDeprecatedBehavior
+    Router.events.on(
+      'routeChangeComplete',
+      this.handleRouteChangeTopLevelRouter
+    )
+
+    console.log(this.state.activeURL);
+    this.props.router.events.on('routeChangeComplete', this.handleRouteChange)
+  }
+
+  componentWillUnmount() {
+    Router.onRouteChangeComplete = null
+    Router.events.off(
+      'routeChangeComplete',
+      this.handleRouteChangeTopLevelRouter
+    )
+    this.props.router.events.off('routeChangeComplete', this.handleRouteChange)
+
+    console.log(this.state.activeURL);
+  }
+
+  handleRouteChange = url => {
+    this.setState({
+      activeURL: url
+    })
+  }
+
+  handleRouteChangeTopLevelRouter = url => {
+    this.setState({
+      activeURLTopLevelRouter: url
+    })
+  }
+
+  handleRouteChangeTopLevelRouterDeprecatedBehavior = url => {
+    this.setState({
+      activeURLTopLevelRouterDeprecatedBehavior: url
+    })
   }
 
   render() {
-    const { Component, pageProps, auth } = this.props;
+    // console.log(this.state.activeURL);
+    const { Component, pageProps, auth, lastPage } = this.props;
+
+    const { activeURL } = this.state;
+
 
     return (
+
       <Container>
+        <div>Active URL: {this.state.activeURL}</div>
         <ToastContainer />
-        <Component {...pageProps} auth={auth} />
+        <Component {...pageProps} auth={auth} lastPage={lastPage} />
       </Container>
     )
   }
